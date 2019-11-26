8aa7e07d0138556725d0846fecd64cd2f2f86e5e
YtalloLima <ytalloaugusto@gmail.com>
2019-11-26 14:28:06 -0300

    adicionando o erd

--- escamboapp/db/schema.rb
+++ escamboapp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_26_001452) do
+ActiveRecord::Schema.define(version: 2019_11_26_171921) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,17 @@ ActiveRecord::Schema.define(version: 2019_11_26_001452) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "ads", force: :cascade do |t|
+    t.string "title", limit: 255
+    t.text "description"
+    t.bigint "category_id"
+    t.bigint "member_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_ads_on_category_id"
+    t.index ["member_id"], name: "index_ads_on_member_id"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -45,4 +56,6 @@ ActiveRecord::Schema.define(version: 2019_11_26_001452) do
     t.index ["reset_password_token"], name: "index_members_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "ads", "categories"
+  add_foreign_key "ads", "members"
 end
