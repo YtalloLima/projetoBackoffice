9f944bcdda3dae4d0b8255ca0240db3ff192b20d
YtalloLima <ytalloaugusto@gmail.com>
2019-11-26 16:06:19 -0300

    configuracao templates comeco

---
+++ escamboapp/app/assets/javascripts/backoffice.js
@@ -0,0 +1,11 @@
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+//= require jquery/dist/jquery
+//= require jquery.easing/jquery.easing
+//= require Chart.min
+//= require chart-area-demo
+//= require chart-pie-demo
+//= require bootstrap/dist/js/bootstrap
+//= require_tree .
+
