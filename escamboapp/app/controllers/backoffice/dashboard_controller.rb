f4752a8532696efb9f54cadca97316bf86cd6437
YtalloLima <ytalloaugusto@gmail.com>
2019-11-27 15:17:19 -0300

    adicionando categorias na tela do site

--- escamboapp/app/controllers/backoffice/dashboard_controller.rb
+++ escamboapp/app/controllers/backoffice/dashboard_controller.rb
@@ -1,7 +1,8 @@
 class Backoffice::DashboardController < ApplicationController
   before_action :authenticate_admin!
   layout "backoffice"
-  def index
 
+ def index
+  	@categories = Category.all
   end
 end
