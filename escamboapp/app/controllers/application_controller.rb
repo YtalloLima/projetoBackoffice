d5390cce2c96d90108e83e37d05e454d697c2ab5
YtalloLima <ytalloaugusto@gmail.com>
2019-11-29 16:52:36 -0300

    Ajeitando as categorias, falta metade da aula

--- escamboapp/app/controllers/application_controller.rb
+++ escamboapp/app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def layout_by_resource
-  	if devise_controller? && resource_ma,e == :admin
+  	if devise_controller? && resource_name == :admin
   		"backoffice_devise"
   	else 
   		"application"
