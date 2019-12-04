7a3a1dcb0364c85dbcc00e755fd1b5eb5525b8fd
YtalloLima <ytalloaugusto@gmail.com>
2019-12-04 15:43:58 -0300

    com o new pronto e com mensagem de confirmação

--- escamboapp/app/controllers/backoffice/categories_controller.rb
+++ escamboapp/app/controllers/backoffice/categories_controller.rb
@@ -4,10 +4,26 @@ class Backoffice::CategoriesController < BackofficeController
   end
 
   def new
+  	@category = Category.new
+  end
+
+  def create
+  	@category = Category.new(params_category)  	
+  	#se salvar, redireciona para a rota abaixo com a mensagem
+  	if @category.save 
+  		redirect_to backoffice_categories_path, notice: "A categoria (#{@category.description}) foi cadastrada com sucesso!"
+  	else
+  	    render new
+    end
+  end
+   
+  def edit
   	
   end
 
-  def edit
-  	
+  private
+
+  def params_category
+  	params.require(:category).permit(:description)
   end
 end
