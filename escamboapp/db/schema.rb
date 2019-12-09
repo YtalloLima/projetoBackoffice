8f018a2a15506de80b0d0d09596ecdd7c8821d63
YtalloLima <ytalloaugusto@gmail.com>
2019-12-09 15:34:25 -0300

    criando campos nome e roles to admin

--- escamboapp/db/schema.rb
+++ escamboapp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_26_171921) do
+ActiveRecord::Schema.define(version: 2019_12_09_182559) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 2019_11_26_171921) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.integer "role"
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
--- escamboapp/lib/tasks/utils.rake
+++ escamboapp/lib/tasks/utils.rake
@@ -5,7 +5,8 @@ namespace :utils do
   	puts "Criando administradores"
 
   	10.times do
-  		Admin.create!(email: Faker::Internet.email,
+  		Admin.create!(name: Faker::Name.name,
+  			email: Faker::Internet.email,
   					   password: "123456",
   					   password_confirmation: "123456")
   	end
